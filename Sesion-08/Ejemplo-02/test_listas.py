2ab8d55d3f22688b8b3feebdcc9d5041f906a356
rctorr <rictor@cuhrt.com>
2021-11-26 05:55:42 -0600

    Actualizando archivos para ejemplo 2 sesion 8

--- Sesion-08/Ejemplo-02/test_listas.py
+++
@@ -1,9 +0,0 @@
-import listas
-
-def test_procesa():
-    assert listas.procesa([]) == []
-    assert listas.procesa([1,2,3,4,5]) == [1,2,3,4,5]
-    assert listas.procesa([1,2,3,4,5,2,4,1]) == [1,2,3,4,5]
-    assert listas.procesa([1,1,1,1,1]) == [1]
-    assert listas.procesa([7,2,9,3,0,1,8,2]) == [0,1,2,3,7,8,9]
-
